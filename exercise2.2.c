93bd419e0886684d232b500d3079358e6f965a69
leoemn <91314367+leoemn@users.noreply.github.com>
2023-07-29 14:46:43 +0100

    Add files via upload

--- exercise2.2.c
+++ exercise2.2.c
@@ -1,8 +1,40 @@
 /*Write a C program to reverse a singly linked list.*/
 
 #include <stdio.h>
+#include <stdlib.h>
+
+//Defining Node structure
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
 
 int main(void)
 {
+    node *list = NULL;
+
+    //Creating a linked list
+    for (int i = 0 ; i < 10 ; i++)
+    {
+        int number;
+        printf("Enter a number to add it in list: ");
+        scanf("%d", &number);
+
+        node *newnode = malloc(sizeof(node));
+        
+        newnode -> data = number;
+        newnode -> next = NULL;
+        newnode -> next = list;
+        list = newnode;
+    }
+    node *tmp = list;
     
+    //Printing linked list without reversing it.
+    printf("Linked list without reversing is: ");
+    while (tmp != NULL)
+    {
+        printf("%d", tmp -> data);
+        tmp = tmp -> next;
+    }
 }
