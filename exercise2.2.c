85c3e1b4ad486b0e21d4b680433288ac4e77a3d7
leoemn <91314367+leoemn@users.noreply.github.com>
2023-07-30 17:12:14 +0100

    Add files via upload

--- exercise2.2.c
+++ exercise2.2.c
@@ -1,5 +1,3 @@
-/*Write a C program to reverse a singly linked list.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,41 +8,49 @@ typedef struct node
     struct node *next;
 }node;
 
-//Functions prototype
+// Functions prototype
 void ReverseLinkedList(node *list);
+void FreeLinkedList(node *list);
 
 int main(void)
 {
     node *list = NULL;
 
-    //Creating a linked list
-    for (int i = 0 ; i < 10 ; i++)
+    // Creating a linked list
+    for (int i = 0; i < 10; i++)
     {
         int number;
         printf("Enter a number to add it in list: ");
         scanf("%d", &number);
 
         node *NewNode = malloc(sizeof(node));
-        
-        NewNode -> data = number;
-        NewNode -> next = NULL;
-        NewNode -> next = list;
+
+        NewNode->data = number;
+        NewNode->next = NULL;
+        NewNode->next = list;
         list = NewNode;
     }
 
-    //Printing linked list without reversing it.
+    // Printing linked list without reversing it.
     node *tmp = list;
     printf("Linked list without reversing is: ");
     while (tmp != NULL)
     {
-        printf("%d", tmp -> data);
-        tmp = tmp -> next;
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
     }
     printf("\n");
+
+    // Reverse the linked list
     ReverseLinkedList(list);
+
+    // Free the memory allocated for the linked list
+    FreeLinkedList(list);
+
+    return 0;
 }
 
-//Function to reverse the linked list
+// Function to reverse the linked list
 void ReverseLinkedList(node *list)
 {
     node *current = list;
@@ -59,12 +65,24 @@ void ReverseLinkedList(node *list)
         current = NextNode;       // Move current to the next node
     }
 
-    //Printing linked list after reversing it.
+    // Printing linked list after reversing it.
     node *tmp = prev;
     printf("Linked list after reversing is: ");
     while (tmp != NULL)
     {
-        printf("%d", tmp -> data);
-        tmp = tmp -> next;
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+
+// Function to free the memory allocated for the linked list
+void FreeLinkedList(node *list)
+{
+    while (list != NULL)
+    {
+        node *nextNode = list->next;
+        free(list); // Free the current node
+        list = nextNode;
     }
 }
