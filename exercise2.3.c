a4557edeb853f7fedc754ef778327865157d1b40
leoemn <91314367+leoemn@users.noreply.github.com>
2023-07-31 18:43:35 +0100

    Add files via upload

---
+++ exercise2.3.c
@@ -0,0 +1,15 @@
+/*Write a C program to find the prime factors of a given number and display them.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    do
+    {
+        printf("Enter number: ");
+        scanf("%d", &num);
+    } while (num <= 0);
+    
+}
